8f2eb31322c505246598ac0f6da2d8fa0d8800be
Claudiu <claudiuferent@gmail.com>
2019-02-10 16:28:28 +0200

    Adding a new class to the project

--- src/test/java/FirstSeleniumTest.java
+++ src/test/java/FirstSeleniumTest.java
@@ -2,9 +2,22 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstSeleniumTest {
+    public static WebDriver driver;
+
+    public static void openBrowser() {
+        System.setProperty("webdriver.chrome.driver", "/home/claudiu/IdeaProjects/SeleniumProject/resources/chromedriver");
+        String startpage = "https://www.google.com";
+        driver = new ChromeDriver();
+        driver.navigate().to(startpage);
+        driver.manage().window().maximize();
+
+
+    }
+
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver","/home/claudiu/IdeaProjects/SeleniumProject/resources/chromedriver");
-        WebDriver driver= new ChromeDriver();
+        openBrowser();
+        Website.Url();
+
 
     }
 }
---
+++ src/test/java/Website.java
@@ -0,0 +1,9 @@
+public class Website extends FirstSeleniumTest {
+
+    public static void Url() {
+
+        driver.navigate().to("https://www.betfair.com");
+    }
+
+
+}
